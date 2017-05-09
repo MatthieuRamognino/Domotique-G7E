666210b609a0655d0f8c0b46a421fde607f98987
MatthieuRamognino <matthieu.ramognino@club-internet.fr>
2017-05-09 12:17:39 +0200

    Add files via upload

--- header2.php
+++ header2.php
@@ -1,15 +1,12 @@
 
 <header id="header2">
 	<div id="conteneur">
-		<div id="conteneur_header2">
-			<a href="Domotique.php"><img class="logo" src="photoSite/maisonvrai4.png" alt="logo"/></a>
-			<div class="nom">
-				<h1>G7 Entreprise</h1>
-			</div>
+		<a href="Domotique.php"><img class="logo" src="Image/maisonvrai4.png" alt="logo"/></a>
+		<div class="nom">
+			G7 Entreprise
 		</div>
 	</div>
-
-	<nav>
+	<nav class="head2">
 		<ul>
 			<li><a href="Domotique.php">Accueil</a></li>
 			<li><a href="#">Contact</a></li>
